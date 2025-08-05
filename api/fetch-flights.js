2df471dd470a61a016929608aec9c4a19f3bc363
han5010 <limhanseo1@gmail.com>
2025-08-04 20:53:23 -0400

    added missing params

--- api/fetch-flights.js
+++ api/fetch-flights.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 export default async function handler(req, res) {
-  const { start, end, date } = req.query;
+  const { start, end, date, gl, hl, currency, type, sort_by, include_airlines } = req.query;
   const apiKey = process.env.apiKey;
 
   if (!apiKey) {
     return res.status(500).json({ error: "Missing SERPAPI_KEY" });
   }
 
-  const url = `https://serpapi.com/search.json?engine=google_flights&departure_id=${start}&arrival_id=${end}&outbound_date=${date}&api_key=${apiKey}`;
+  const url = `https://serpapi.com/search.json?engine=google_flights&departure_id=${start}&arrival_id=${end}&gl=${gl}&hl=${hl}&currency=${currency}&outbound_date=${date}&type=${type}&sort_by=${sort_by}&include_airlines=${include_airlines}&api_key=${apiKey}`;
 
   try {
     const response = await axios.get(url);
